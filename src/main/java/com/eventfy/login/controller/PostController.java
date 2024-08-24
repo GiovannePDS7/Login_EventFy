817c287868b99da36c36954e37c80ac980d7330f
Giovanne Pagano <giovanne3282@gmail.com>
2024-08-23 22:57:03 -0300

    continuação

--- src/main/java/com/eventfy/login/controller/PostController.java
+++ src/main/java/com/eventfy/login/controller/PostController.java
@@ -1,10 +1,48 @@
 package com.eventfy.login.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.eventfy.login.repository.PostRepository;
+import com.eventfy.login.entity.Post;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
 
+    @Autowired
+    private PostRepository repository;
+
+    @GetMapping
+    public List<Post> listarPosts(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> buscarPorId(@PathVariable Long id){
+        var resposta = repository.findById(id);
+        if (resposta.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }else {
+            return ResponseEntity.ok(resposta.get());
+        }
+    }
+    public ResponseEntity<Post> inserir(@RequestBody Post post){
+        repository.save(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(post));
+    }
+
+    public ResponseEntity<Post> atualizar(@PathVariable long id, @RequestBody Post post){
+        var existe = repository.findById(id);
+        if(!existe.isPresent())
+            return  ResponseEntity.notFound().build();
+        return ResponseEntity.ok(repository.save(post));
+    }
 }
--- src/main/java/com/eventfy/login/entity/Post.java
+++ src/main/java/com/eventfy/login/entity/Post.java
@@ -1,12 +1,26 @@
 package com.eventfy.login.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "TBL_LOGIN2")
+
 public class Post {
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
 }
