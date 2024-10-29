db2707cde6d8ff5ed7054b270be699f96f0cf4ff
Etec Prof. Horácio Augusto da Silveira <giovanne3282@gmail.com>
2024-10-29 11:09:35 -0300

    alterações em sala

--- src/main/java/com/eventfy/login/config/SecurityConfiguration.java
+++ src/main/java/com/eventfy/login/config/SecurityConfiguration.java
@@ -1,11 +1,7 @@
 package com.eventfy.login.config;
 
-import com.eventfy.login.service.OrganizadorService; // Importe sua classe de UserDetailsService
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,9 +16,6 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-
-    @Autowired
-    private OrganizadorService userDetailsService; // Injetando o UserDetailsService
 
     @Bean
     public PasswordEncoder passwordEncoder() {
