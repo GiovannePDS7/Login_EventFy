f99de31ea0a025ef2ae5f14ff435a82f9c3928ad
giovanne <giovanne3282@gmail.com>
2024-11-04 11:43:15 -0300

    desenvolvendo login

--- src/main/java/com/eventfy/login/controller/LoginController.java
+++ src/main/java/com/eventfy/login/controller/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
     private OrganizadorService organizadorService;
 
 
-    @PostMapping
+    @PostMapping("/authenticate")
     public LoginResponseDTO authenticate(@RequestBody OrganizadorAuth organizadorAuth) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
