6fcaea3f48a7f14aa7747b433e1b3c45fccda764
giovanne <giovanne3282@gmail.com>
2024-11-04 11:08:25 -0300

    desenvolvendo login

--- src/main/java/com/eventfy/login/controller/LoginController.java
+++ src/main/java/com/eventfy/login/controller/LoginController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/organizadores")
 public class LoginController {
 
     @Autowired
