19da8fed5ccb9ae4228eede3a88f9865c1351158
giovanne <giovanne3282@gmail.com>
2024-11-04 11:03:26 -0300

    desenvolvendo login

--- src/main/java/com/eventfy/login/repository/OrganizadorRepository.java
+++ src/main/java/com/eventfy/login/repository/OrganizadorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OrganizadorRepository extends JpaRepository<Organizador, Long> {
-    Optional<Organizador> findByEmailOrganizador(String email);
+    Optional<Organizador> findByEmailOrganizador(String emailOrganizador);
 }
